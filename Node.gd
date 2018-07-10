4521c12351354d873dd98425d33471f806fc4887
Brandon Makin <bmicmak@gmail.com>
2018-07-10 15:17:27 -0700

    added "CONNECT_DEFERRED" to signals
    CONNECT_DEFERRED added to prevent the game freezing from an apparent futex deadlock

--- Node.gd
+++ Node.gd
@@ -3,20 +3,22 @@ extends Node2D
 var pc1
 var pc2
 
+var show_debug_messages = true;
+
 func _ready():
 	
 	print("--start--")	
 	pc1 = WebRTCPeer.new();
 	pc2 = WebRTCPeer.new();
 	
-	pc1.connect("notify", self, "pc1_notifications");
-	pc2.connect("notify", self, "pc2_notifications");
+	pc1.connect("notify", self, "pc1_notifications", [], CONNECT_DEFERRED);
+	pc2.connect("notify", self, "pc2_notifications", [], CONNECT_DEFERRED);
 	
-	pc1.connect("new_ice_candidate", self, "pc1_has_ice_candidate")
-	pc2.connect("new_ice_candidate", self, "pc2_has_ice_candidate")
+	pc1.connect("new_ice_candidate", self, "pc1_has_ice_candidate", [], CONNECT_DEFERRED)
+	pc2.connect("new_ice_candidate", self, "pc2_has_ice_candidate", [], CONNECT_DEFERRED)
 	
-	pc1.connect("offer_created", self, "send_offer_1_to_2");
-	pc2.connect("offer_created", self, "send_offer_2_to_1");
+	pc1.connect("offer_created", self, "send_offer_1_to_2", [], CONNECT_DEFERRED);
+	pc2.connect("offer_created", self, "send_offer_2_to_1", [], CONNECT_DEFERRED);
 
 	print("--host--")
 
@@ -27,12 +29,19 @@ func my_method(msg):
 	print(msg);
 
 func pc1_notifications(msg):
-	print("pc1 message: " + msg);
-	#pass
+	# This is for DEBUG. It notifies of all DEBUG messages
+	# and is not used for the signaling process.  
+	
+	if show_debug_messages:
+		print("pc1 message: " + msg);
+	
 
 func pc2_notifications(msg):
-	print("pc2 message: " + msg);
-	#pass
+	# This is for DEBUG. It notifies of all DEBUG messages
+	# and is not used for the signaling process.
+
+	if show_debug_messages:
+		print("pc2 message: " + msg);
 
 func send_offer_1_to_2(type, sdp):
 	pc2.set_remote_description(sdp, true)
@@ -50,13 +59,17 @@ func _input(event):
 		pc1.get_state_peer_connection()
 
 func pc1_has_ice_candidate(MidName, MlineIndexName, Name):
-	print("pc1 ice candidate: MidName = " + MidName \
-	 + ", MLineIndexName = " + str(MlineIndexName) \
-	 + ", Name = " + Name);
 	pc2.add_ice_candidate(MidName, MlineIndexName, Name);
+	
+	if show_debug_messages:
+		print("pc1 ice candidate: MidName = " + MidName \
+		 + ", MLineIndexName = " + str(MlineIndexName)  \
+		 + ", Name = " + Name);
 
 func pc2_has_ice_candidate(MidName, MlineIndexName, Name ):
-	print("pc2 ice candidate: MidName = " + MidName \
-	 + ", MLineIndexName = " + str(MlineIndexName) \
-	 + ", Name = " + Name);
 	pc1.add_ice_candidate(MidName, MlineIndexName, Name);
+
+	if show_debug_messages:
+		print("pc2 ice candidate: MidName = " + MidName \
+		 + ", MLineIndexName = " + str(MlineIndexName)  \
+		 + ", Name = " + Name);
